74365da6b0adbbdddc00be22612e98f64936305b
bmm522 <rlawldls11@naver.com>
2023-02-26 17:50:17 +0900

    init

---
+++ src/main/java/com/jobseecker/jobseeckerstudio/JobSeeckerStudioApplication.java
@@ -0,0 +1,13 @@
+package com.jobseecker.jobseeckerstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeeckerStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeeckerStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobseecker/jobseeckerstudio/JobSeeckerStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseecker.jobseeckerstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeeckerStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
