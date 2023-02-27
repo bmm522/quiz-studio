d07f5d7652fff4753830d40bd6b134b1c2b7df79
bmm522 <rlawldls11@naver.com>
2023-02-27 22:07:50 +0900

    feat : security config 파일추가

---
+++ src/main/java/com/jobseecker/jobseeckerstudio/login/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.jobseecker.jobseeckerstudio.login.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService()
+                .and()
+                .successHandler()
+
+    }
+}
