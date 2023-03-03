b20848b86ed5b3207a6a579ecf648a17a13cad3a
bmm522 <rlawldls11@naver.com>
2023-03-03 14:52:36 +0900

    feat : BaseTimeEntity 추가

---
+++ src/main/java/com/jobseecker/jobseeckerstudio/global/entity/BaseTimeEntity.java
@@ -0,0 +1,18 @@
+package com.jobseecker.jobseeckerstudio.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    public LocalDateTime createDate;
+}
