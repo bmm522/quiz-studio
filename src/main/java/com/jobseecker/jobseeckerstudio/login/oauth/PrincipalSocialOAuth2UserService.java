9deb13d4284f403d3b60c0c6b6cd31d7bf9650c0
bmm522 <rlawldls11@naver.com>
2023-03-08 21:13:58 +0900

    feat : Principal Oatuh2 서비스 객체 추가

---
+++ src/main/java/com/jobseecker/jobseeckerstudio/login/oauth/PrincipalSocialOAuth2UserService.java
@@ -0,0 +1,8 @@
+package com.jobseecker.jobseeckerstudio.login.oauth;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+
+public class PrincipalSocialOAuth2UserService extends DefaultOAuth2UserService {
+
+
+}
