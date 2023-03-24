a7e762c34eaf8e8a74b0e8c6710601ca41c6ad82
bmm522 <rlawldls11@naver.com>
2023-03-24 17:56:38 +0900

    feat : 소셜 컨트롤러 추가

---
+++ src/main/java/com/jobseeckerstudio/bmm522/user/controller/LoginApiController.java
@@ -0,0 +1,16 @@
+package com.jobseeckerstudio.bmm522.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1")
+@Controller
+public class LoginApiController {
+
+    @GetMapping("/{social}")
+    public String moveSocialLoginForm(@PathVariable("social")String social){
+        return "redirect:/oauth2/authorization/"+social;
+    }
+}
