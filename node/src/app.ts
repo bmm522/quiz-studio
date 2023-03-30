31de26df446ab5392093e651fc2c3b801bec2231
bmm522 <rlawldls11@naver.com>
2023-03-30 16:03:37 +0900

    delete : node 백서버 프론트요소 삭제

--- node/src/app.ts
+++ node/src/app.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import Container from "typedi";
 import { useContainer as routingUseContainer, useExpressServer } from "routing-controllers";
 import * as bodyParser from "body-parser";
-import path from "path";
-import {IndexController} from "./controller/index/IndexController";
 import {createDatabaseConnection} from "./config/database";
 import {QuestionController} from "./controller/question/QuestionController";
 
@@ -13,12 +11,8 @@ export class App {
 
   constructor() {
     this.app = express();
-    this.app.set('views', path.join(__dirname, 'view/ejs'));
-    this.app.set('view engine', 'ejs');
-
     this.setExpress();
     this.setMiddlewares();
-
   }
 
   private setExpress(): void {
@@ -26,7 +20,7 @@ export class App {
       routingUseContainer(Container);
       useExpressServer(this.app, {
         routePrefix: "/api/v1",
-        controllers: [IndexController, QuestionController]
+        controllers: [QuestionController]
       })
     }catch (error) {
       console.log(error);
@@ -44,19 +38,5 @@ export class App {
   private setMiddlewares(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
-    // CSS 및 JavaScript 파일에 대한 경로 설정
-    this.app.use('/css', express.static(path.join(__dirname, 'view/ejs/css')));
-    this.app.use('/js', express.static(path.join(__dirname, 'view/ejs/js')));
-    this.app.use('/image', express.static(path.join(__dirname, 'view/ejs/image')));
-
-    // CSS 및 JavaScript 파일의 MIME 유형 설정
-    this.app.use('*.css', (req, res, next) => {
-      res.set('Content-Type', 'text/css');
-      next();
-    });
-    this.app.use('*.js', (req, res, next) => {
-      res.set('Content-Type', 'text/javascript');
-      next();
-    });
   }
 }
--- node/src/view/ejs/js/loginPage.js
+++
@@ -1,9 +0,0 @@
-const host = 'http://localhost:8081';
-
-document.getElementById('google-connect').addEventListener('click', function(){
-    location.href = `${host}/api/v1/login/google`;
-})
-
-document.getElementById('kakao-connect').addEventListener('click', function(){
-    location.href = `${host}/api/v1/login/kakao`;
-})
