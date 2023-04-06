3ac2db96137666977d3c04a6ce9d859504029deb
bmm522 <rlawldls11@naver.com>
2023-04-06 16:52:24 +0900

    feat : 프론트 단 문제 제출 후 맞은개수, 다시풀기, 메인메뉴 가는 버튼 생기도록 구현

--- front/public/js/quiz.js
+++ front/public/js/quiz.js
@@ -1,5 +1,6 @@
 const loginHost = "http://localhost:8081";
 const nodeHost = "http://localhost:3000";
+const frontHost = "http://localhost:3001";
 
 
 const level = localStorage.getItem('level');
@@ -67,12 +68,11 @@ window.onload = function () {
   });
 
 }
-
-
 document.getElementById('submit-btn').addEventListener('click', function() {
   const result = confirm('퀴즈를 제출하시겠습니까?');
   if (result) {
     const cardBodys = document.getElementsByClassName('card-body');
+    let numCorrect = 0;
     for (let i = 0; i < cardBodys.length; i++) {
       const cardBody = cardBodys[i];
       const choiceInputs = cardBody.getElementsByClassName('form-check-input');
@@ -85,12 +85,12 @@ document.getElementById('submit-btn').addEventListener('click', function() {
           if (choiceInput.value === 'true') {
             choiceInput.parentNode.classList.add('bg-success'); // 정답인 경우 배경색을 초록색으로 변경
             isCorrect = true;
+            numCorrect++;
           } else {
             choiceInput.parentNode.classList.add('bg-danger'); // 오답인 경우 배경색을 빨간색으로 변경
           }
         } else if (choiceInput.value === 'true') {
-          console.log('여기');
-          choiceInput.parentNode.classList.add('bg-success'); // 선택하지 않았지만 정답인 경우 텍스트 색상을 초록색으로 변경
+          choiceInput.parentNode.classList.add('bg-warning'); // 선택하지 않았지만 정답인 경우 텍스트 색상을 초록색으로 변경
         }
       }
       if (isCorrect) {
@@ -99,5 +99,28 @@ document.getElementById('submit-btn').addEventListener('click', function() {
         quizTitle.classList.add('text-danger'); // 오답인 경우 제목의 텍스트 색상을 빨간색으로 변경
       }
     }
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn.style.display = 'none';
+    const navbar = document.getElementById('after');
+    const score = document.createElement('span');
+    score.classList.add('mr-2', 'd-none', 'd-lg-inline', 'text-gray-600', 'small');
+    score.innerText = `맞은 개수: ${numCorrect}/10`;
+    const retryBtn = document.createElement('button');
+    retryBtn.classList.add('btn', 'btn-primary', 'btn-sm', 'mr-2');
+    retryBtn.innerText = '다시 풀기';
+    retryBtn.addEventListener('click', function() {
+      window.location.reload();
+    });
+    const mainMenuBtn = document.createElement('button');
+    mainMenuBtn.classList.add('btn', 'btn-secondary', 'btn-sm');
+    mainMenuBtn.innerText = '메인 메뉴로';
+    mainMenuBtn.addEventListener('click', function() {
+      window.location.href = `${frontHost}/main`;
+    });
+    navbar.appendChild(score);
+    navbar.appendChild(retryBtn);
+    navbar.appendChild(mainMenuBtn);
+    score.classList.remove('d-none');
+    window.scrollTo(0, 0);
   }
 });
