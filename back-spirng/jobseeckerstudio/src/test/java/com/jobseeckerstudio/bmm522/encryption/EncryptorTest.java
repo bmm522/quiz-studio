e1984662da8da421b05cceefc84a767cb47c24ce
bmm522 <rlawldls11@naver.com>
2023-04-14 15:16:35 +0900

    test : 복호화 테스트코드 추가

--- back-spirng/jobseeckerstudio/src/test/java/com/jobseeckerstudio/bmm522/encryption/EncryptorTest.java
+++ back-spirng/jobseeckerstudio/src/test/java/com/jobseeckerstudio/bmm522/encryption/EncryptorTest.java
@@ -1,5 +1,6 @@
 package com.jobseeckerstudio.bmm522.encryption;
 
+import com.jobseeckerstudio.bmm522.global.exception.DecryptionException;
 import com.jobseeckerstudio.bmm522.user.encryption.Decryptor;
 import com.jobseeckerstudio.bmm522.user.encryption.Encryptor;
 import com.jobseeckerstudio.bmm522.user.encryption.properties.EncryptionProperties;
@@ -34,4 +35,27 @@ public class EncryptorTest {
         assertNotEquals(encryptedText, plainText);
     }
 
+    @Test
+    @DisplayName("복호화 테스트 - 성공")
+    void testDecrypt_success() {
+        // given
+        String plainText = "test string";
+        String encryptedText = Encryptor.encrypt(plainText);
+
+        // when
+        String decryptedText = Decryptor.decrypt(encryptedText);
+
+        // then
+        assertEquals(plainText, decryptedText);
+    }
+
+    @Test
+    @DisplayName("복호화 테스트 - 예외 발생")
+    void testDecrypt_failure() {
+        // given
+        String encryptedText = "invalid-encrypted-string";
+
+        // when and then
+        assertThrows(DecryptionException.class, () -> Decryptor.decrypt(encryptedText));
+    }
 }
