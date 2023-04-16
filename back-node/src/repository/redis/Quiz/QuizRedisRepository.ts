df0e6efdf379165b5484ce8addaf111429cfe8d8
bmm522 <rlawldls11@naver.com>
2023-04-17 07:52:16 +0900

    test : redis Test

--- back-node/src/repository/redis/Quiz/QuizRedisRepository.ts
+++ back-node/src/repository/redis/Quiz/QuizRedisRepository.ts
@@ -4,32 +4,38 @@ import { env } from "../../../config/env";
 
 @Service()
 export class QuizRedisRepository {
-    redisClient = new Redis({
-        host: env.redis.host as string,
-        port: parseInt(env.redis.port as string),
-        username: env.redis.username as string,
-        password: env.redis.password as string,
-    });
+
 
     key: string = "test";
     data: string = "ggg";
 
     async findByCategoryNameAndDifficulty(
+
         categoryName: string,
         difficulty: string
     ) {
+        const redisClient = new Redis({
+            host: env.redis.host as string,
+            port: parseInt(env.redis.port as string),
+            username: env.redis.username as string,
+            password: env.redis.password as string,
+        });
         console.log(categoryName);
         console.log(difficulty);
         const redisKey = `${categoryName}_${difficulty}`;
         let redisValue;
+        let redisData;
 
         try {
-            redisValue = await this.redisClient.keys('*');
+            redisValue = await redisClient.keys('*');
+            redisData = await redisClient.hgetall('quiz');
+            console.log(redisValue.length);
             console.log(redisValue);
+            console.log("redisdata : " + redisData);
         } catch (err) {
             console.error(err);
         } finally {
-            await this.redisClient.quit(); // Redis 클라이언트 명시적으로 닫아줌
+            await redisClient.quit(); // Redis 클라이언트 명시적으로 닫아줌
         }
     }
 }
--- back-spirng/jobseeckerstudio/quiz/src/main/java/quiz/model/redisQuiz/mapper/RedisMapper.java
+++ back-spirng/jobseeckerstudio/quiz/src/main/java/quiz/model/redisQuiz/mapper/RedisMapper.java
@@ -3,23 +3,27 @@ package quiz.model.redisQuiz.mapper;
 import quiz.model.quiz.repository.dto.QuizDto;
 import quiz.model.redisQuiz.RedisQuiz;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class RedisMapper {
 
     public static List<RedisQuiz> toRedisQuiz(List<QuizDto> quizDtoList) {
+        AtomicInteger i = new AtomicInteger();
         return quizDtoList.stream().map(quizDto -> {
             List<RedisQuiz.RedisQuizChoices> redisQuizChoicesList = quizDto.getChoiceDtos().stream()
                 .map(choiceDto -> new RedisQuiz.RedisQuizChoices(choiceDto.getChoiceContent(), choiceDto.isAnswer()))
                 .collect(Collectors.toList());
 
-            return RedisQuiz.builder()
-                .id(quizDto.getCategoryName() + "_" + quizDto.getDifficulty())
+            RedisQuiz redisQuiz =  RedisQuiz.builder()
+                .id(quizDto.getCategoryName() + "_" + quizDto.getDifficulty() + i)
                 .quizTitle(quizDto.getQuizTitle())
                 .quizChoices(redisQuizChoicesList)
                 .build();
+
+            i.getAndIncrement();
+            return redisQuiz;
         }).collect(Collectors.toList());
     }
 }
