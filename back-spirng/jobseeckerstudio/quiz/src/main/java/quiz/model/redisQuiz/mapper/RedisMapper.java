e1f49ee8e89fef3041132dcb5a73ee2e86229e1d
bmm522 <rlawldls11@naver.com>
2023-04-18 11:19:29 +0900

    refactor : redisMapper 코드분리

--- back-spirng/jobseeckerstudio/quiz/src/main/java/quiz/model/redisQuiz/mapper/RedisMapper.java
+++ back-spirng/jobseeckerstudio/quiz/src/main/java/quiz/model/redisQuiz/mapper/RedisMapper.java
@@ -11,19 +11,28 @@ public class RedisMapper {
 
     public static List<RedisQuiz> toRedisQuiz(List<QuizDto> quizDtoList) {
         AtomicInteger i = new AtomicInteger();
-        return quizDtoList.stream().map(quizDto -> {
-            List<RedisQuiz.RedisQuizChoices> redisQuizChoicesList = quizDto.getChoiceDtos().stream()
-                .map(choiceDto -> new RedisQuiz.RedisQuizChoices(choiceDto.getChoiceContent(), choiceDto.isAnswer()))
-                .collect(Collectors.toList());
+        return quizDtoList.stream()
+            .map(quizDto -> toRedisQuiz(quizDto, i.getAndIncrement()))
+            .collect(Collectors.toList());
+    }
 
-            RedisQuiz redisQuiz =  RedisQuiz.builder()
-                .id(quizDto.getCategoryName() + "_" + quizDto.getDifficulty() + i)
-                .quizTitle(quizDto.getQuizTitle())
-                .quizChoices(redisQuizChoicesList)
-                .build();
+    private static String generateQuizId(QuizDto quizDto, int index) {
+        return quizDto.getCategoryName() + "_" + quizDto.getDifficulty() + index;
+    }
 
-            i.getAndIncrement();
-            return redisQuiz;
-        }).collect(Collectors.toList());
+    private static RedisQuiz toRedisQuiz(QuizDto quizDto, int index) {
+        List<RedisQuiz.RedisQuizChoices> redisQuizChoicesList = toRedisQuizChoicesList(quizDto.getChoiceDtos());
+
+        return RedisQuiz.builder()
+            .id(generateQuizId(quizDto, index))
+            .quizTitle(quizDto.getQuizTitle())
+            .quizChoices(redisQuizChoicesList)
+            .build();
+    }
+
+    private static List<RedisQuiz.RedisQuizChoices> toRedisQuizChoicesList(List<QuizDto.ChoiceDto> choiceDtoList) {
+        return choiceDtoList.stream()
+            .map(choiceDto -> new RedisQuiz.RedisQuizChoices(choiceDto.getChoiceContent(), choiceDto.isAnswer()))
+            .collect(Collectors.toList());
     }
 }
