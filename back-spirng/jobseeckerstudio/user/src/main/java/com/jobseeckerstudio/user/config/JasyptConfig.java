96df73c56ff5832b903801e0201bf5def9ae4200
bmm522 <rlawldls11@naver.com>
2023-04-30 23:56:00 +0900

    feat : .yml 설정 파일 암호화 한 후 추가

---
+++ back-spirng/jobseeckerstudio/user/src/main/java/com/jobseeckerstudio/user/config/JasyptConfig.java
@@ -0,0 +1,32 @@
+package com.jobseeckerstudio.user.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String secretKey;
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(secretKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
--- back-spirng/jobseeckerstudio/user/src/test/java/com/jobseeckerstudio/user/JobSeeckerStudioUserApplicationTests.java
+++ back-spirng/jobseeckerstudio/user/src/test/java/com/jobseeckerstudio/user/JobSeeckerStudioUserApplicationTests.java
@@ -1,10 +1,13 @@
 package com.jobseeckerstudio.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class JobSeeckerStudioUserApplicationTests {
+
 
 	@Test
 	void contextLoads() {
