3fe6408f6b25f3e1b7c1a1d88fbf8155d535a382
bmm522 <rlawldls11@naver.com>
2023-05-16 18:03:04 +0900

    fix : build fail fix

--- api-spring/jobseeckerstudio/quiz/src/test/java/quiz/JobSeeckerStudioQuizApplicationTests.java
+++ api-spring/jobseeckerstudio/quiz/src/test/java/quiz/JobSeeckerStudioQuizApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
-@EnableJpaAuditing
 public class JobSeeckerStudioQuizApplicationTests {
 
     @Test
