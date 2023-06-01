c192bb84797f9a5d104557b30a61adfb4440a6b7
bmm522 <rlawldls11@naver.com>
2023-06-01 21:24:54 +0900

    refactor : 카테고리 주석 추가

--- spring-module/quizstudio/quiz/src/main/java/quiz/controller/category/CategoryController.java
+++ spring-module/quizstudio/quiz/src/main/java/quiz/controller/category/CategoryController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import quiz.controller.category.dto.CategorySaveBody;
 import quiz.controller.category.dto.CategoryUpdateBody;
@@ -25,39 +24,63 @@ public class CategoryController {
 
 	private final CategoryService categoryService;
 
+	/**
+	 * 카테고리 저장 메서드
+	 *
+	 * @param userKey 사용자 키
+	 * @param request 카테고리 저장 요청 정보
+	 * @return CommonResponse 객체
+	 */
 	@PostMapping("/category")
-	public @ResponseBody CommonResponse<?> saveCategory(
-		@RequestAttribute("userKey") String userKey,
-		@RequestBody CategorySaveBody request) {
+	public CommonResponse<?> saveCategory(
+		@RequestAttribute("userKey") final String userKey,
+		@RequestBody final CategorySaveBody request) {
 		return ResponseHandler.handle(HttpStatus.CREATED.value(), "카테고리 저장 성공",
-			categoryService.save(
-				CategoryConverter.toSaveRequest(userKey, request)));
+			categoryService.save(CategoryConverter.toSaveRequest(userKey, request)));
 	}
 
+	/**
+	 * 카테고리 조회 메서드
+	 *
+	 * @param userKey 사용자 키
+	 * @param page    페이지 번호
+	 * @return CommonResponse 객체
+	 */
 	@GetMapping("/category")
 	public CommonResponse<?> getCategories(
-		@RequestAttribute("userKey") String userKey,
-		@RequestParam("page") int page) {
-		System.out.println("여기는들어오면안돼ㅑ");
+		@RequestAttribute("userKey") final String userKey,
+		@RequestParam("page") final int page) {
 		return ResponseHandler.handle(HttpStatus.OK.value(), "카테고리 불러오기 성공",
 			categoryService.get(userKey, page));
 	}
 
+	/**
+	 * 카테고리 옵션 조회 메서드
+	 *
+	 * @param userKey 사용자 키
+	 * @return CommonResponse 객체
+	 */
 	@GetMapping("/category-option")
 	public CommonResponse<?> getCategoriesWhenOption(
-		@RequestAttribute("userKey") String userKey
+		@RequestAttribute("userKey") final String userKey
 	) {
-		System.out.println("여기로들어옴?");
 		return ResponseHandler.handle(HttpStatus.OK.value(), "카테고리 불러오기 성공(옵션)",
 			categoryService.getCategoriesWhenSelectOption(userKey));
 	}
 
+	/**
+	 * 카테고리 업데이트 메서드
+	 *
+	 * @param userKey 사용자 키
+	 * @param request 카테고리 업데이트 요청 정보
+	 * @return CommonResponse 객체
+	 */
 	@PatchMapping("/category")
-	public CommonResponse<?> updateCategories(@RequestAttribute("userKey") String userKey,
-		@RequestBody CategoryUpdateBody request) {
+	public CommonResponse<?> updateCategories(
+		@RequestAttribute("userKey") final String userKey,
+		@RequestBody final CategoryUpdateBody request) {
 		return ResponseHandler.handle(HttpStatus.OK.value(), "카테고리 업데이트 성공",
-			categoryService.update(
-				CategoryConverter.toUpdateReqeust(userKey, request)));
+			categoryService.update(CategoryConverter.toUpdateRequest(userKey, request)));
 	}
 
 }
--- spring-module/quizstudio/quiz/src/main/java/quiz/controller/category/util/CategoryConverter.java
+++ spring-module/quizstudio/quiz/src/main/java/quiz/controller/category/util/CategoryConverter.java
@@ -7,12 +7,24 @@ import quiz.global.exception.InvalidParameterFromDtoException;
 import quiz.service.category.dto.CategorySaveParam;
 import quiz.service.category.dto.CategoryUpdateParam;
 
+/**
+ * 해당 클래스는 컨트롤러의 DTO를 서비스의 DTO로 변환해주는 기능을 제공합니다.
+ */
 public class CategoryConverter {
 
+	/**
+	 * CategorySaveParam.Request 객체를 생성하여 반환하는 메서드입니다.
+	 *
+	 * @param userKey 사용자 키
+	 * @param request 카테고리 저장 요청 정보
+	 * @return CategorySaveParam.Request 객체
+	 * @throws InvalidParameterFromDtoException save 요청의 title이나 description이 유효하지 않을 때 예외 발생
+	 * @throws ExistCategorySaveException       중복된 카테고리 이름이 있을 때 예외 발생
+	 */
 	public static CategorySaveParam.Request toSaveRequest(final String userKey,
 		final CategorySaveBody request) {
-		validateTitleAndDescription(request.getTitle(), request.getDescription());
-		checkExistCategoryName(request.getTitle());
+		validateTitleAndDescription(request.getTitle(), request.getDescription()); // 제목과 설명 유효성 검사
+		checkExistCategoryName(request.getTitle()); // 중복 카테고리 이름 검사
 		return CategorySaveParam.Request.builder()
 			.userKey(userKey)
 			.title(request.getTitle())
@@ -20,9 +32,17 @@ public class CategoryConverter {
 			.build();
 	}
 
-	public static CategoryUpdateParam.Request toUpdateReqeust(final String userKey,
+	/**
+	 * CategoryUpdateParam.Request 객체를 생성하여 반환하는 메서드입니다.
+	 *
+	 * @param userKey 사용자 키
+	 * @param request 카테고리 업데이트 요청 정보
+	 * @return CategoryUpdateParam.Request 객체
+	 * @throws ExistCategorySaveException 중복된 카테고리 이름이 있을 때 예외 발생
+	 */
+	public static CategoryUpdateParam.Request toUpdateRequest(final String userKey,
 		final CategoryUpdateBody request) {
-		checkExistCategoryName(request.getUpdateTitle());
+		checkExistCategoryName(request.getUpdateTitle()); // 중복 카테고리 이름 검사
 		return CategoryUpdateParam.Request.builder()
 			.categoryId(request.getCategoryId())
 			.userKey(userKey)
@@ -31,6 +51,13 @@ public class CategoryConverter {
 			.build();
 	}
 
+	/**
+	 * 제목과 설명이 유효한지 검사하는 메서드입니다.
+	 *
+	 * @param title       제목
+	 * @param description 설명
+	 * @throws InvalidParameterFromDtoException save 요청의 title이나 description이 유효하지 않을 때 예외 발생
+	 */
 	private static void validateTitleAndDescription(final String title, final String description) {
 		if (title == null || title.isEmpty()) {
 			throw new InvalidParameterFromDtoException("save 요청에 title이 담기지 않았습니다.");
@@ -39,15 +66,20 @@ public class CategoryConverter {
 		if (description == null || description.isEmpty()) {
 			throw new InvalidParameterFromDtoException("save 요청에 description이 담기지 않았습니다.");
 		}
-
 	}
 
+	/**
+	 * 카테고리 이름이 중복되는지 검사하는 메서드입니다.
+	 *
+	 * @param title 카테고리 제목
+	 * @throws ExistCategorySaveException 기존의 카테고리 이름과 중복될 때 예외 발생
+	 */
 	private static void checkExistCategoryName(final String title) {
 		String checkStr = title.toLowerCase().trim().replace(" ", "");
 
-		if ("java".equals(checkStr) || "datastructure".equals(checkStr)) {
+		if ("java".equals(checkStr) || "datastructure".equals(checkStr) || "database".equals(
+			checkStr)) {
 			throw new ExistCategorySaveException("기존의 카테고리 이름은 사용할 수 없습니다.");
 		}
 	}
-
 }
