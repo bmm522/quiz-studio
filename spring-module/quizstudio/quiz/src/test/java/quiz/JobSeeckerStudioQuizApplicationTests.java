167091351b424d03db87613987e797c561a71f72
bmm522 <rlawldls11@naver.com>
2023-06-02 18:17:22 +0900

    fix : run_env 값 삭제

--- spring-module/quizstudio/quiz/src/test/java/quiz/JobSeeckerStudioQuizApplicationTests.java
+++ spring-module/quizstudio/quiz/src/test/java/quiz/JobSeeckerStudioQuizApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class JobSeeckerStudioQuizApplicationTests {
+public class
+JobSeeckerStudioQuizApplicationTests {
 
 	@Test
 	void contextLoads() {
