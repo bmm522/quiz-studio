ba9bbc1ea75d3ce17debf3ab6d6b81ed1c6fb940
bmm522 <rlawldls11@naver.com>
2023-06-07 12:53:12 +0900

    fix : quiz-node module cors 설정에 origin 명시적으로 추가 #7

--- node-module/src/app.ts
+++ node-module/src/app.ts
@@ -42,9 +42,12 @@ export class App {
   }
 
   private setMiddlewares(): void {
+    const corsOptions = {
+      origin: 'https://www.quizstudio.site'
+    };
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
-    this.app.use(cors());
+    this.app.use(cors(corsOptions));
     this.app.use(JwtAuthorizationFilter);
     this.app.use(ErrorHandler);
   }
