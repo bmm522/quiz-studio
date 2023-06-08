63e98d74c74e1b42dd320d3c940fe4cbc1a59412
bmm522 <rlawldls11@naver.com>
2023-06-08 11:19:22 +0900

    !HOTFIX : quiz-batch module에서 레디스 삭제, 저장 스텝 나누고 simplejob 에서 flowjob으로 변경 #17

--- spring-module/quizstudio/quiz-batch/src/main/java/com/quizbatch/tasklets/saveredis/step1clearAll/ClearQuizAtRedisTasklet.java
+++ spring-module/quizstudio/quiz-batch/src/main/java/com/quizbatch/tasklets/saveredis/step1clearAll/ClearQuizAtRedisTasklet.java
@@ -1,32 +1,21 @@
-package com.quizbatch.tasklets.saveredis.step1save;
+package com.quizbatch.tasklets.saveredis.step1clearAll;
 
-import com.quizbatch.domain.quiz.QuizQueryDto;
 import com.quizbatch.domain.quiz.QuizRedisRepository;
-import com.quizbatch.domain.quiz.QuizRepository;
-import com.quizbatch.domain.quizschema.QuizSchema;
-import com.quizbatch.domain.quizschema.QuizSchemaMapper;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
-
 @Component
 @RequiredArgsConstructor
-@Slf4j
-public class SaveQuizAtRedisTasklet implements Tasklet {
-
+public class ClearQuizAtRedisTasklet implements Tasklet {
 
 	private final QuizRedisRepository quizRedisRepository;
 
-	private final QuizRepository quizRepository;
-
 	/**
-	 * Redis에 퀴즈를 저장하는 Tasklet입니다. RDBMS에서 퀴즈 목록을 가져와서 Redis에 저장합니다.
+	 * 기존의 Redis안에 있는 퀴즈를 삭제하는 Tasklet입니다.
 	 *
 	 * @param stepContribution StepContribution 객체
 	 * @param chunkContext     ChunkContext 객체
@@ -36,10 +25,7 @@ public class SaveQuizAtRedisTasklet implements Tasklet {
 	@Override
 	public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
 		throws Exception {
-		List<QuizQueryDto> quizzes = quizRepository.getQuizzesForRedisBy();
-		List<QuizSchema> quizSchemas = QuizSchemaMapper.toQuizSchemas(quizzes);
 		quizRedisRepository.deleteAll();
-		quizRedisRepository.saveAll(quizSchemas);
 		return RepeatStatus.FINISHED;
 	}
 }
---
+++ spring-module/quizstudio/quiz-batch/src/main/java/com/quizbatch/tasklets/saveredis/step2save/SaveQuizAtRedisTasklet.java
@@ -0,0 +1,44 @@
+package com.quizbatch.tasklets.saveredis.step2save;
+
+import com.quizbatch.domain.quiz.QuizQueryDto;
+import com.quizbatch.domain.quiz.QuizRedisRepository;
+import com.quizbatch.domain.quiz.QuizRepository;
+import com.quizbatch.domain.quizschema.QuizSchema;
+import com.quizbatch.domain.quizschema.QuizSchemaMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SaveQuizAtRedisTasklet implements Tasklet {
+
+
+	private final QuizRedisRepository quizRedisRepository;
+
+	private final QuizRepository quizRepository;
+
+	/**
+	 * Redis에 퀴즈를 저장하는 Tasklet입니다. RDBMS에서 퀴즈 목록을 가져와서 Redis에 저장합니다.
+	 *
+	 * @param stepContribution StepContribution 객체
+	 * @param chunkContext     ChunkContext 객체
+	 * @return RepeatStatus.FINISHED
+	 * @throws Exception 예외 발생 시
+	 */
+	@Override
+	public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
+		throws Exception {
+		List<QuizQueryDto> quizzes = quizRepository.getQuizzesForRedisBy();
+		List<QuizSchema> quizSchemas = QuizSchemaMapper.toQuizSchemas(quizzes);
+		quizRedisRepository.saveAll(quizSchemas);
+		return RepeatStatus.FINISHED;
+	}
+}
