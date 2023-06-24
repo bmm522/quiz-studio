6e1b8b199dbbf487510a2b9c055ed184f521fe91
bmm522 <rlawldls11@naver.com>
2023-06-24 23:43:34 +0900

    fix : 레디스 작업 시간 변경 후 다시 빌드

--- spring-module/quizstudio/quiz-batch/src/main/java/com/quizbatch/schedulers/SaveRedisScheduler.java
+++ spring-module/quizstudio/quiz-batch/src/main/java/com/quizbatch/schedulers/SaveRedisScheduler.java
@@ -22,7 +22,7 @@ public class SaveRedisScheduler {
 	/**
 	 * 매일 5시 30분에 db에 저장된 데이터를 레디스로 저장하는 스케줄링 메서드입니다.
 	 */
-	@Scheduled(cron = "0 48 2 * * *")
+	@Scheduled(cron = "0 50 2 * * *")
 	public void saveQuizAtRedisJobSchedule() {
 		try {
 			jobLauncher.run(
