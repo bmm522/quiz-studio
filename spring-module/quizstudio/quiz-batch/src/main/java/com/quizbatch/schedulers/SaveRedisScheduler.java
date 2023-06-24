ce1566cf4d7abeabeb734af0843fac324731b0b7
bmm522 <rlawldls11@naver.com>
2023-06-25 00:07:56 +0900

    fix : redis 작업 utc 시간 기준으로 스케줄링 테스트

--- spring-module/quizstudio/quiz-batch/src/main/java/com/quizbatch/schedulers/SaveRedisScheduler.java
+++ spring-module/quizstudio/quiz-batch/src/main/java/com/quizbatch/schedulers/SaveRedisScheduler.java
@@ -22,7 +22,7 @@ public class SaveRedisScheduler {
 	/**
 	 * 매일 5시 30분에 db에 저장된 데이터를 레디스로 저장하는 스케줄링 메서드입니다.
 	 */
-	@Scheduled(cron = "0 2 3 * * *")
+	@Scheduled(cron = "0 14 15 * * *")
 	public void saveQuizAtRedisJobSchedule() {
 		try {
 			jobLauncher.run(
