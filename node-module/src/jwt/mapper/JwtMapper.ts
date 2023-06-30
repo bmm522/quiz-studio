4f5674c85e651c24a714dd70754a8c5f20a4d78d
bmm522 <rlawldls11@naver.com>
2023-06-30 18:35:40 +0900

    feat : quiz-node module jwt 필터 쿠키방식으로 변경 #46

--- node-module/src/jwt/mapper/JwtMapper.ts
+++ node-module/src/jwt/mapper/JwtMapper.ts
@@ -3,9 +3,32 @@ import { JwtToken } from '../dto/JwtToken';
 
 export class JwtMapper {
   static async toJwtToken(req: Request): Promise<JwtToken> {
+    const jwtToken: string =  await this.getCookieValue(req, "Authorization");
+    const refreshToken: string = await  this.getCookieValue(req, "RefreshToken");
+    jwtToken.replace("+", " ");
+    refreshToken.replace("+", " ");
     return new JwtToken(
-      typeof req.headers.authorization === 'string' ? req.headers.authorization : undefined,
-      typeof req.headers.refreshtoken === 'string' ? req.headers.refreshtoken : undefined,
+     jwtToken,refreshToken
     );
   }
+
+  static async getCookieValue(req: Request, cookieName: string) : Promise<string>{
+    const cookies = req.cookies;
+    let cookieValue = '';
+
+    if (cookieName && cookies && typeof cookies === 'object') {
+      for (const name in cookies) {
+        if (Object.prototype.hasOwnProperty.call(cookies, name) && name === cookieName) {
+          cookieValue = cookies[name];
+          break;
+        }
+      }
+    }
+
+    if (!cookieValue) {
+      cookieValue = 'guest';
+    }
+
+    return cookieValue;
+}
 }
