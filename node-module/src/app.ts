2699896d4fba30b42824296849dfc4c57617ce42
bmm522 <rlawldls11@naver.com>
2023-06-30 19:49:05 +0900

    fix : credentials 옵션 임시로 삭제 test #46

--- node-module/src/app.ts
+++ node-module/src/app.ts
@@ -44,7 +44,6 @@ export class App {
   private setMiddlewares(): void {
     const corsOptions = {
       origin: 'https://www.quizstudio.site',
-      credentials: true
     };
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
